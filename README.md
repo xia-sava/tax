fc70ceff55d4ca1c596c149553de37d2f74534e9
ISIGE, Akira <xia@silvia.com>
2019-02-05 11:42:17 +0900

    excel_to_pdf.py

---
+++ README.md
@@ -0,0 +1,12 @@
+確定申告の資料整理ツールの類
+
+----
+
+excel_to_pdf.py:
+
+    請求書の Excel ファイルとかをまとめて PDF にするやつ
+
+amazon-csv\extract.py:
+
+    Firefoxアマゾン注文履歴フィルタのCSVを請求月ごとにアレするやつ 
+
--- excel_to_pdf.py
+++ excel_to_pdf.py
@@ -8,13 +8,25 @@ def main():
     excel = win32com.client.Dispatch("Excel.Application")
     excel.Visible = False
 
-    for wb_name in sys.argv[1:]:  # type: str
-        wb_path = Path(wb_name)
-        pdf_path = Path(wb_name.replace('xlsx', 'pdf'))
+    try:
+        for wb_name in sys.argv[1:]:  # type: str
+            print(f'Processing: {wb_name}')
+            wb_name = wb_name.replace('\\\\', '\\').replace('/', '\\')
+            wb_path = Path(wb_name).absolute()
+            pdf_path = Path(wb_name.replace('xlsx', 'pdf')).absolute()
+            if pdf_path.exists():
+                pdf_path.unlink()
 
-        wb = excel.Workbooks.Open(wb_path)
-        wb.WorkSheets(1).Select()
-        wb.ActiveSheet.ExportAsFixedFormat(0, pdf_path)
+            wb = excel.Workbooks.Open(str(wb_path))
+            try:
+                wb.WorkSheets(1).Select()
+                wb.ActiveSheet.ExportAsFixedFormat(0, str(pdf_path))
+            finally:
+                wb.Close(True)
+                wb = None
+    finally:
+        excel.Quit()
+        excel = None
 
 
 if __name__ == '__main__':
