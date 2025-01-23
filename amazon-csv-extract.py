e5e002686703f6d4a4f9767485d4593f1f46948e
ISIGE, Akira <xia@silvia.com>
2025-01-23 20:34:56 +0900

    2025年版

--- amazon-csv-extract.py
+++ amazon-csv-extract.py
@@ -1,63 +1,120 @@
 import csv
-import sys
-from datetime import datetime
+import re
 from pathlib import Path
+
+import pdfplumber
 
 # language=markdown
 """
-1. Amazon履歴フィルタでクレカ請求に入ってきてそうな月の範囲を CSV 出力して c:/tmp/A に配置
-2. 出力された extracted_all.csv を見てこれはちゃうやろってのを excluded_order に並べて繰り返す
-3. 注文履歴フィルタを全月分にして criteria.txt の1行ずつで絞り込み，領収証を PDF 出力する
-4. 年初の注文分は年を跨ぐので前年分も出すこと
+1. アマゾン注文適格請求書＆領収書一括取得で請求書 pdf を出力して c:/tmp/A に展開
+2. 同範囲の CSV を出力して c:/tmp/A に展開
+3. このスクリプトを実行して extracted_all.csv を出力
+4. extracted_all.csv を見て先頭カラムに，消耗品費に 1 を，非対象に 0 を入れる
+5. 再度このスクリプトを実行して extracted_all.csv を読み込んで分類していく
+
+
 """
 
 
+DATA_DIR = Path("c:/tmp/A")
+EXTRACT_CSV = DATA_DIR / "extracted_all.csv"
+CREDIT_CARD_LAST4 = "2600"
+
+
 def main():
-    credit_card_last4s = ['2600']
-    year = datetime.now().year
-    m_range = [datetime(year - 2, 12, 4)] + [datetime(year - 1, m, 1) for m in range(1, 13)]
-    excluded_order = [
-        "503-5028732-6403838",
-        "503-6473318-0367046",
-    ]
+    if not EXTRACT_CSV.exists():
+        extract_pdf()
+    else:
+        distribute_files()
 
-    data_dir = Path(sys.argv[1] if len(sys.argv) > 1 else '.')
-    data = [[] for _ in range(12)]
-    for csvfile in sorted(data_dir.glob('amazon-order_*.csv')):
-        with open(csvfile, 'r', encoding='utf-8') as w_month:
-            reader = csv.reader(w_month)
-            header = next(reader)
-            incomp_data: list[list] = []
-            for row in reader:
-                order_no = row[1]
-                name = row[2]
-                if name == '（クレジットカードへの請求）':
-                    charge_date_str = row[12]
-                    charge_date = datetime.strptime(charge_date_str, '%Y/%m/%d')
-                    month = [i for i in range(12) if m_range[i] <= charge_date < m_range[i + 1]]
-                    if not month:
-                        continue
-                    for record in incomp_data:
-                        data[month[0]].append([charge_date_str] + record)
-                    incomp_data = []
-                    continue
-                author = row[3]
-                if row[4] == '':
-                    continue
-                price = int(row[4])
-                credit_card = row[14]
-                if order_no in excluded_order:
-                    continue
-                if any(f'下4けたが{last4}' in credit_card for last4 in credit_card_last4s):
-                    incomp_data.append([order_no, price, name, author])
 
-    with open(data_dir / 'extracted_all.csv', 'w', newline='', encoding='utf_8_sig') as w_all:
-        with open(data_dir / 'criteria.txt', 'w') as w_cri:
-            for m, month_data in enumerate(data):
-                month_data.sort(key=lambda r: r[0])
-                csv.writer(w_all).writerows(month_data)
-                print(' OR '.join([r[1] for r in month_data]), file=w_cri)
+def extract_pdf():
+    exclude_orders = []
+    for csv_file in DATA_DIR.iterdir():
+        if re.match(r"order.*.csv", csv_file.name):
+            with csv_file.open() as f:
+                reader = csv.reader(f)
+                next(reader)
+                for row in reader:
+                    if CREDIT_CARD_LAST4 not in row[5]:
+                        exclude_orders.append(row[0])
+    results = []
+    for pdf_file in DATA_DIR.iterdir():
+        if pdf_file.suffix == ".pdf":
+            print(f"Extracting data from {pdf_file}")
+            ret = parse_invoice_data(pdf_file)
+            if ret is None:
+                continue
+            if ret[3] in exclude_orders:
+                continue
+            results.append(["", *ret, pdf_file.name])
+    with EXTRACT_CSV.open("w", newline="\n") as output:
+        writer = csv.writer(output)
+        writer.writerow(["分類", "請求書発行日", "合計金額", "購入内容", "注文番号", "ファイル名"])
+        writer.writerows(sorted(results, key=lambda x: x[1]))
 
+
+def distribute_files():
+    with EXTRACT_CSV.open() as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            month = row[1][:7]
+            match row[0]:
+                case "1":
+                    dest_dir = mkdir(month, "消耗品費")
+                case "":
+                    dest_dir = mkdir(month, "書籍")
+                case _:
+                    continue
+            pdf = DATA_DIR / row[5]
+            pdf.rename(dest_dir / pdf.name)
+            print(f"Moved {pdf} to {dest_dir}/{pdf.name}")
+
+
+def mkdir(month, category):
+    month_dir = DATA_DIR / month
+    if not month_dir.exists():
+        month_dir.mkdir()
+    category_dir = month_dir / category
+    if not category_dir.exists():
+        category_dir.mkdir()
+    return category_dir
+
+
+def parse_invoice_data(pdf_path):
+    invoice_date = None
+    total_amount = None
+    purchase_content = None
+    order_number = None
+
+    with pdfplumber.open(pdf_path) as pdf:
+        all_text = []
+        for page in pdf.pages:
+            extracted = page.extract_text()
+            if extracted:
+                all_text.append(extracted)
+        text = "\n".join(all_text)
+
+    if re.search(r"合計 -￥", text):
+        return None
+
+    if m := re.search(r"請求(?:書発行)?日 ([-0-9]+)", text):
+        invoice_date = m.group(1)
+
+    if m := re.search(r"合計 ￥([\d,]+)", text):
+        total_amount = m.group(1).replace(",", "")
+
+    if m := re.search(r"税抜 税込 税込\n(.+) [\d,]+ ￥[\d,]+ [\d,]+% ￥[\d,]+ ￥[\d,]+", text):
+        purchase_content = m.group(1)
+
+    if m := re.search(r"注文番号\s*([-0-9]+)", text):
+        order_number = m.group(1)
+
+    if not all([invoice_date, total_amount, purchase_content, order_number]):
+        raise ValueError(f"Failed to extract all necessary data from {pdf_path}")
+
+    return invoice_date, total_amount, purchase_content, order_number
 
 if __name__ == '__main__':
     main()
